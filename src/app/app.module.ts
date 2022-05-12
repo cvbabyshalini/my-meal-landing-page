23dc0892d90eb018cf5c719438335b1348b98f35
Baby Shalini <chowlababy@gmailcom>
2022-05-12 09:51:43 +0530

    add all components and front view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { FeaturedFoodComponent } from './components/featured-food/featured-food.component';
+import { SocialFeedComponent } from './components/social-feed/social-feed.component';
+import { ChefsComponent } from './components/chefs/chefs.component';
+import { OurProductsComponent } from './components/our-products/our-products.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent,
+    FeaturedFoodComponent,
+    SocialFeedComponent,
+    ChefsComponent,
+    OurProductsComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/chefs/chefs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { chefs } from '../../mocks/index';
+@Component({
+  selector: 'app-chefs',
+  templateUrl: './chefs.component.html',
+  styleUrls: ['./chefs.component.css']
+})
+export class ChefsComponent implements OnInit {
+  chefsList = chefs;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
